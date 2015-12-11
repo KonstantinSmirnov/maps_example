50d365f5b849758315e390c57ea07c7efba9649f
konstantinsmirnov <rusksmi@gmail.com>
2015-12-11 08:19:45 +0300

    Add user ceate method and bootstrap errors messaging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path, notice: "User was successfully created." }
+      else
+        format.html { redirect_to root_path, notice: "User not created." }
+      end
+    end
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :latitude, :longitude)
+    end
 end
